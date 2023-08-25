38c398043e0bf5ce8cf1e2feb764b4cc64c99ff8
ilmiraA <ilmiraa2007@gamil.com>
2023-08-25 17:14:21 +0300

    сделана кнопка отправки

--- Script/index.js
+++ Script/index.js
@@ -1,13 +1,6 @@
 function fetchData() {
-    let unit_function = document.getElementById('unit_function').value;
-    let unit_type = document.getElementById('unit_type').value;
-    let unit_name = document.getElementById('unit_name').value;
-    let certificate = document.getElementById('certificate').value;
-    let gen_power = document.getElementById('gen_power').value;
-    let battery_capacity = document.getElementById('battery_capacity').value;
-    let installation_date = document.getElementById('installation_date').value;
-    let service_life = document.getElementById('service_life').value;
-    let device_status = document.getElementById('device_status').value;
+    let data_container = document.getElementById('data_container').childNodes;
+    let res = [];
 
     let unit_function_option = {
         "Не выбрано": "not_selected",
@@ -33,21 +26,38 @@ function fetchData() {
         "В ремонте": "under_repair", 
     }
 
-    unit_function = unit_function_option[unit_function]
-    unit_type = unit_type_option[unit_type]
-    device_status = device_status_option[device_status]
+    for (let i = 0; i < data_container.length; i++)
+    {
+        let ch = data_container[i];
 
-    let data = {
-        unit_function: unit_function,
-        unit_type: unit_type,
-        unit_name: unit_name,
-        certificate: certificate,
-        gen_power: gen_power,
-        battery_capacity: battery_capacity,
-        installation_date: installation_date,
-        service_life: service_life,
-        device_status: device_status,
+        let unit_function = ch.querySelector('#unit_function').value;
+        let unit_type = ch.querySelector('#unit_type').value;
+        let unit_name = ch.querySelector('#unit_name').value;
+        let certificate = ch.querySelector('#certificate').value;
+        let gen_power = ch.querySelector('#gen_power').value;
+        let battery_capacity = ch.querySelector('#battery_capacity').value;
+        let installation_date = ch.querySelector('#installation_date').value;
+        let service_life = ch.querySelector('#service_life').value;
+        let device_status = ch.querySelector('#device_status').value;
+
+        unit_function = unit_function_option[unit_function];
+        unit_type = unit_type_option[unit_type];
+        device_status = device_status_option[device_status];
+
+        let data = {
+            unit_function: unit_function,
+            unit_type: unit_type,
+            unit_name: unit_name,
+            certificate: certificate,
+            gen_power: gen_power,
+            battery_capacity: battery_capacity,
+            installation_date: installation_date,
+            service_life: service_life,
+            device_status: device_status,
+        }
+
+        res.push(data);
     }
 
-    return data;
+    return res;
 }
